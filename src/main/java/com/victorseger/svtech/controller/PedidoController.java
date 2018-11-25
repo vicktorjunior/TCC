ab9c25df5002b313b61f1351a90e895277172b0a
Victor Seger <vicktor.junior@gmail.com>
2018-11-24 23:10:30 -0200

    fixed bug when opening new order

--- src/main/java/com/victorseger/svtech/controller/PedidoController.java
+++ src/main/java/com/victorseger/svtech/controller/PedidoController.java
@@ -89,7 +89,7 @@ public class PedidoController {
         model.addAttribute("order", pedido);
         model.addAttribute("action", "new");
         model.addAttribute("items", new HashSet<>());
-        model.addAttribute("products", productService.findWithUnits());
+        model.addAttribute("products", productService.findAll());
         model.addAttribute("newItem", itemPedido);
         return new ModelAndView("order/items/form");
     }
